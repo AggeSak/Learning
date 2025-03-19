f35701615499644ed06656beb7831ec583467abb
Aggelos Sakalis <aggelossakalis8@gmail.com>
2025-03-19 20:48:12 +0200

    Python Basics

---
+++ Python/Data structures.py
@@ -0,0 +1,31 @@
+
+# Online Python - IDE, Editor, Compiler, Interpreter
+
+dictionary ={
+    "a":"alpha",
+    "b":"byta",
+    "c":"ciiii"
+}
+print("This is the Dictionary:")
+for x in dictionary:
+    a = x
+
+    print(a,end=" : ")
+    print(dictionary[a])
+
+print("This is our List:")    
+List=[1,2,3,4]
+k = len(List)
+
+for i in range(k):
+    print(List[i],end=" ")
+
+print("This is our Tuple:")
+
+Pleiada_Tuple=(1,2,a,5)
+
+print(Pleiada_Tuple[2])
+
+
+a = "hello"
+print(a[2])
