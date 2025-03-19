51704b94cfdb993e776b1583817c060c9158358c
Aggelos Sakalis <aggelossakalis8@gmail.com>
2025-03-19 20:57:20 +0200

    Basic Java

--- Java/Main.java
+++ Java/Main.java
@@ -1,5 +1,17 @@
 class Main {
     public static void main(String[] args){
         System.out.println("Hello World!");
+        Hello_World_Again obj = new Hello_World_Again();
+        obj.function();
     }
+    
+}
+
+class Hello_World_Again{
+
+    public void function(){
+        System.out.println("Hello World Again!");
+    }
+
+
 }
